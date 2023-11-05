efe13cefd44059487f66376ff2ee5b9d3c94edd5
zalish-shawon <zalish.shawon@gmail.com>
2023-11-05 16:01:48 +0600

    posted card added

--- src/components/PostedJobs/PostedJobsCard.jsx
+++ src/components/PostedJobs/PostedJobsCard.jsx
@@ -6,18 +6,32 @@ const PostedJobsCard = ({ job }) => {
 
         <div>
             <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <img class="w-full h-[200px]" src={image} alt="Sunset in the mountains"/>
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">{jobTitle}</div>
-                        <p class="text-gray-700 text-base">
-                            {description.slice(0,250)}
-                        </p>
-                    </div>
-                    <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                    </div>
+                <img class="w-full h-[200px]" src={image} alt="Sunset in the mountains" />
+                <div class="px-6 py-2">
+                    <div class="font-bold text-xl mb-2">{jobTitle}</div>
+                    <p class="text-gray-700 text-base">
+                        {description.slice(0, 150)}<span><a className="text-blue-400">...read more</a></span>
+                    </p>
+                    
+                </div>
+                <div className="flex items-center">
+                <p className="font-semibold text-red-500 px-6">Deadline: {deadline}</p>
+                <div class="badge badge-primary badge-outline">{category}</div>                
+                </div>
+                <div className="flex gap-1 items-center px-6 mt-2">
+                    <p className="font-semibold text-gray-500">Min price</p> <span class="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">${minPrice}</span>
+                    <p className="font-semibold text-gray-500">Max price</p>
+                <span class="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">${maxPrice}</span>
+                
+                </div>
+                <div class="px-6 pt-4 pb-2 flex gap-3">
+                    <button type="button" class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
+                        Delete
+                    </button>
+                    <button type="button" class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-green-500 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
+                        Update
+                    </button>
+                </div>
             </div>
         </div>
 
