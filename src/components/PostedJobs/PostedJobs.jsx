2c59d04d4f9e51497da32c01477cc5dd3a78f3a7
zalish-shawon <zalish.shawon@gmail.com>
2023-11-05 15:11:49 +0600

    posted jobs added

--- src/components/PostedJobs/PostedJobs.jsx
+++ src/components/PostedJobs/PostedJobs.jsx
@@ -1,8 +1,37 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import PostedJobsCard from "./PostedJobsCard";
 
 const PostedJobs = () => {
+    const {user} = useContext(AuthContext);
+    const [postedJobs, setPostedjobs] = useState([]);
+    
+    useEffect(()=>{
+        try {
+            axios.get('http://localhost:5000/jobs')
+            .then(res => setPostedjobs(res.data))
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [])
+
+    const myJobs = postedJobs.filter(item => item.email === user?.email);
+
+
+
+
+
     return (
         <div>
-            
+             <div>
+                <h1 className="text-4xl font-bold mt-10 text-center">My Posted <span className="text-[orangered]">Jobs</span></h1>
+                <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[1200px] mx-auto py-8">
+                    {
+                        myJobs.map(job => <PostedJobsCard key={job._id} job={job}></PostedJobsCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
